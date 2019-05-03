542ea5dae5f6b22f05ce5d8bf1e67437bdb2ae92
GlennvanWezel <32335925+GlennvanWezel@users.noreply.github.com>
2019-05-03 14:35:06 +0200

    Firebase AuthUI toegeovegd

--- app/src/main/java/com/example/mi4/ui/ItemsActivity.kt
+++ app/src/main/java/com/example/mi4/ui/ItemsActivity.kt
@@ -1,5 +1,7 @@
 package com.example.mi4.ui
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
@@ -10,6 +12,8 @@ import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.example.mi4.R
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_items.*
 
 class ItemsActivity : AppCompatActivity() {
@@ -41,6 +45,25 @@ class ItemsActivity : AppCompatActivity() {
                 .setAvailableProviders(providers)
                 .build(), RC_SIGN_IN, null
         )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
