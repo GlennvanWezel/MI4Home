7af5a0e70852dbfd218d1cd80328ced1dede82ca
GlennvanWezel <32335925+GlennvanWezel@users.noreply.github.com>
2019-06-05 21:53:19 +0200

    ItemRepo getCurrentItems ingevuld maar met code in commentaar

--- app/src/main/java/com/example/mi4/data/repository/ItemRepositoryImpl.kt
+++ app/src/main/java/com/example/mi4/data/repository/ItemRepositoryImpl.kt
@@ -1,20 +1,36 @@
 package com.example.mi4.data.repository
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.mi4.data.db.entity.Item
 import com.example.mi4.data.ItemDao
 import com.example.mi4.data.network.ItemNetworkDataSource
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.*
 
-class ItemRepositoryImpl (
+class ItemRepositoryImpl(
     private val itemDao: ItemDao,
     private val itemNetworkDataSource: ItemNetworkDataSource
-    ): ItemRepository {
+) : ItemRepository {
 
     init {
         itemNetworkDataSource.toString()
     }
 
     override suspend fun getCurrentItems(userID: String): LiveData<List<Item>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//        GlobalScope.launch(Dispatchers.IO) {
+////            var user = FirebaseFirestore
+////                .getInstance()
+////                .collection("users")
+////                .document(
+////                    FirebaseAuth
+////                        .getInstance()
+////                        .currentUser!!
+////                        .uid
+////                )
+////                .get()
+////        }
+        return MutableLiveData<List<Item>>()
     }
 }
