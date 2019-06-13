a02b1e1e5211045ce0a802d03b2d68c700e2815b
GlennvanWezel <32335925+GlennvanWezel@users.noreply.github.com>
2019-06-14 00:18:12 +0200

    Made a settings switch which switches between Dutch and English
    Added locales for dutch and english in values strings.xml AND translated all strings

--- app/src/main/java/com/example/mi4/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/mi4/ui/settings/SettingsFragment.kt
@@ -1,7 +1,20 @@
 package com.example.mi4.ui.settings
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
+import com.example.mi4.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat(){
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.preferences)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        (activity as AppCompatActivity)?.supportActionBar?.title = getString(R.string.settings)
+        (activity as AppCompatActivity)?.supportActionBar?.subtitle = null
+
+    }
+
 }
